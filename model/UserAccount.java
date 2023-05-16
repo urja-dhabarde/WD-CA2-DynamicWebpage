66242408bbdf3ed2050d700c7608d7c28e1854a2
urja-dhabarde <112320215+urja-dhabarde@users.noreply.github.com>
2023-05-16 21:59:56 +0530

    Add files via upload

---
+++ model/UserAccount.java
@@ -0,0 +1,29 @@
+package example.model;
+
+public class UserAccount {
+
+private String email;
+private String password;
+private String secretcode;
+
+
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getSecretcode() {
+	return secretcode;
+}
+public void setSecretcode(String secretcode) {
+	this.secretcode = secretcode;
+}
+
+}
